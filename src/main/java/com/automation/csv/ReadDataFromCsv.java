4ddfd665f6978abc765f3e4426c5f7c304437d9f
QA-Sarah-tester <mssarah870@gmail.com>
2026-03-08 00:27:06 +0530

    Update ReadDataFromCsv.java
    .

--- src/main/java/com/automation/csv/ReadDataFromCsv.java
+++ src/main/java/com/automation/csv/ReadDataFromCsv.java
@@ -1,14 +1,11 @@
 package com.automation.csv;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-
 public class ReadDataFromCsv {
-	
 	public static void main(String[] args) {
 		
 		File csvFile = new File(System.getProperty("user.dir") + "\\src\\test\\resources\\CreateUser.csv");
@@ -25,8 +22,5 @@ public class ReadDataFromCsv {
 			System.out.println("Name: " +createUser.get("Name"));
 			System.out.println("Job: " +createUser.get("Job"));
 		}
-		 
 	}
-	
-
 }
